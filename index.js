df8395b44410fba97d6180732c5527d3feece02a
Daniel <jilek.d@northeastern.edu>
2025-11-18 14:48:25 -0500

    Basic skeleton of title page

---
+++ index.js
@@ -0,0 +1,4 @@
+import {View} from "/view.js"
+
+let view = new View();
+view.renderTitlePage();
---
+++ view.js
@@ -0,0 +1,30 @@
+import { TitlePage } from "./titlepage.js"
+
+class View {
+    constructor() {
+
+    }
+
+    clearPage() {
+        while (this.container.firstChild) {
+            this.container.lastChild.remove();
+        }
+    }
+
+    renderTitlePage() {
+        this.clearPage();
+        const titlePage = new TitlePage(this, this.container);
+        titlePage.render();
+    }
+
+    renderFlightsMenu() {
+    }
+
+    renderCustomersMenu() {}
+
+    renderStaffMenu() {}
+
+    renderAircraftMenu() {}
+}
+
+export {View}
